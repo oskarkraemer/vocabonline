2bf094a213d107025e9a7f226d693a8824182a84
Oskar Krämer <42745862+oskarkraemer@users.noreply.github.com>
2024-02-12 00:57:45 +0100

    change link in footer to point to my gh

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     return (
         <footer className="m-4 sticky top-[100vh]">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span className="text-sm text-muted-foreground sm:text-center">© {new Date().getFullYear()} <a href="https://flowbite.com/" className="hover:underline">Oskar Krämer</a>. All Rights Reserved.
+            <span className="text-sm text-muted-foreground sm:text-center">© {new Date().getFullYear()} <a href="https://github.com/oskarkraemer" className="hover:underline">Oskar Krämer</a>. All Rights Reserved.
             </span>
             <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-muted-foreground sm:mt-0">
                 <li>
