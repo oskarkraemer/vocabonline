83143007499f68ed5d0004be4d7a729ca5536313
Oskar Krämer <42745862+oskarkraemer@users.noreply.github.com>
2024-02-27 21:38:07 +0100

    add Docker

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:17-jdk-alpine
+MAINTAINER oskarkraemer
+
+VOLUME /tmp
+COPY out/artifacts/vocabonline_jar/vocabonline.jar vocabonline.jar
+ENTRYPOINT ["java","-jar","/vocabonline.jar"]
--- src/main/java/me/oskarkraemer/vocabonline/model/meaning/MeaningRepository.java
+++ src/main/java/me/oskarkraemer/vocabonline/model/meaning/MeaningRepository.java
@@ -1,6 +1,8 @@
 package me.oskarkraemer.vocabonline.model.meaning;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MeaningRepository extends JpaRepository<Meaning, Long> {
 }
